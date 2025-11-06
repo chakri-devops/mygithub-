2379865fd4b516d0dc475d479a33733fdab4d74a
chakri-devops <nallurichakravarthi6@gmail.com>
2025-11-05 22:01:30 -0800

    code added java file

--- app.java
+++ app.java
@@ -3,3 +3,13 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+#dev2 added the code
+    public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
